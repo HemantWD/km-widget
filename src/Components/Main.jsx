8d61900fbc7ee2be257f76f6794656b6c19c4557
HemantWD <hemantajaysharma@gmail.com>
2023-02-23 11:01:15 +0530

    changes

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -19,6 +19,7 @@ export default function Main() {
     const mphCallBack = (speed) => {
         setSpeedObject({
             kmph: speed*1.6,
+            // this formula will change the speed into mph
             mph: speed 
         })
     }
